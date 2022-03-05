fc5fed3ccacc08349ab93990793e27af3ac84a97
abanobmorgan <abanob.k.morgan@gmail.com>
2022-03-05 14:43:25 +0200

    edit the fetching file data

--- notebook/datafetching.py
+++ notebook/datafetching.py
@@ -2,20 +2,25 @@ import json
 import pandas as pd 
 import numpy as np 
 import requests
+import time
 
 
-df = pd.read_csv("doc/dialect_dataset.csv")
+df = pd.read_csv("../doc/dialect_dataset.csv")
 df.id=df.id.astype(str)
 ids = df.id.array
 ids= list(ids)
 
-x = open("data/output.txt","w", encoding="UTF-8")
+x = open("../data/output1.txt","w", encoding="UTF-8")
 
 url = 'https://recruitment.aimtechnologies.co/ai-tasks'
 for i in range(0,len(ids),1000):
-    payload = ids[:i]
+    payload = ids[i:i+1]
     data=json.dumps(payload)
+    time.sleep(2)  
     r = requests.post(url, data=data)
+    if(r.ok): 
+        x.write(r.text[1:-1])
+        
 
-    x.write(r.text)
 x.close()
+print("done")
