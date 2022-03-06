6f7b955943ba73798e09d2d4ada6caf4573be967
abanobmorgan <abanob.k.morgan@gmail.com>
2022-03-06 15:58:45 +0200

    final fetching code file

--- notebook/datafetching.py
+++ notebook/datafetching.py
@@ -2,24 +2,31 @@ import json
 import pandas as pd
 import requests
 import time
-
+import re
 
 df = pd.read_csv("../doc/dialect_dataset.csv")
 df.id=df.id.astype(str)
 ids = df.id.array
 ids= list(ids)
 
-x = open("../data/output1.txt","w", encoding="UTF-8")
+x = open("../data/finaloutput.txt","w", encoding="UTF-8")
 
 url = 'https://recruitment.aimtechnologies.co/ai-tasks'
-for i in range(0,len(ids),441):
-    payload = ids[i:i+1]
+for i in range(0,len(ids),1000):
+    payload = ids[i:i+1000]
     data=json.dumps(payload)
     time.sleep(2)  
+    print(i)
     r = requests.post(url, data=data)
     if(r.ok): 
-        x.write(r.text[1:-1])
-        
+        data =eval(r.text) 
+        for key, value in data.items(): 
+            value = re.sub(r",",'',value)
+            value = re.sub(r"\s+",'',value)
+            x.write('%s,%s\n' % (key, value))
+    else : 
+        print(f"error in id = : {i}")
+                
 
 x.close()
 print("done")
